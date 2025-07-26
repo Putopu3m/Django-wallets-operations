92a1ab6882f0707f0788c54b50346232b8bed4dd
Putopu3m <necro9990@gmail.com>
2025-07-26 17:52:40 +0300

    Changed BankAccount model to Wallet model. Created appy_operation function

--- backend/wallets/models.py
+++ backend/wallets/models.py
@@ -4,7 +4,7 @@ import uuid
 
 User = get_user_model()
 
-class BankAccount(models.Model):
+class Wallet(models.Model):
     account_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     amount = models.DecimalField(max_digits=20, decimal_places=2, default=0)
     users = models.ManyToManyField(User, related_name='accounts')
@@ -20,7 +20,7 @@ class Operation(models.Model):
     operation_type = models.CharField(choices=OperationChoices.choices)
     amount = models.DecimalField(max_digits=20, decimal_places=2, default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='operations')
-    bank_account = models.ForeignKey(BankAccount, on_delete=models.CASCADE, related_name='operations')
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE, related_name='operations')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
---
+++ backend/wallets/services.py
@@ -0,0 +1,30 @@
+from decimal import Decimal
+from django.db import transaction
+from rest_framework.exceptions import ValidationError
+from .models import Operation, BankAccount
+
+
+def apply_operation(account: BankAccount, user, operation_type: str, amount: Decimal) -> Decimal:
+    with transaction.atomic():
+        account = BankAccount.objects.select_for_update().get(pk=account.pk)
+
+        if operation_type == Operation.OperationChoices.WITHDRAW:
+            if account.amount < amount:
+                raise ValidationError("Insufficient funds.")
+
+            account.amount -= amount
+        elif operation_type == Operation.OperationChoices.DEPOSIT:
+            account.amount += amount
+        else:
+            raise ValidationError("Invalid operation type.")
+
+        account.save()
+
+        Operation.objects.create(
+            bank_account=account,
+            user=user,
+            operation_type=operation_type,
+            amount=amount
+        )
+
+        return account.amount
