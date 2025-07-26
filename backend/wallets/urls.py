1addc8502905bdcbb5328febf1c01f69a8a6b79e
Putopu3m <necro9990@gmail.com>
2025-07-26 18:17:14 +0300

    Fixed wallets paths

--- backend/wallets/urls.py
+++ backend/wallets/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import WalletDetailView, WalletOperationView
 
 urlpatterns = [
-    path("api/v1/wallets/<uuid:uuid>", WalletDetailView.as_view(), name="wallet-detail"),
-    path("api/v1/wallets/<uuid:uuid>/operation", WalletOperationView.as_view(), name="wallet-operation"),
+    path("api/v1/wallets/<uuid:wallet_id>", WalletDetailView.as_view(), name="wallet-detail"),
+    path("api/v1/wallets/<uuid:wallet_id>/operation", WalletOperationView.as_view(), name="wallet-operation"),
 ]
