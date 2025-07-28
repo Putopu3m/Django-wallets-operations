7a573b1502633b471c22f5dc9b4a9d5d609a311d
Putopu3m <necro9990@gmail.com>
2025-07-28 19:59:05 +0300

    Removed HTTP404 from wallets/views.py

--- backend/wallets/views.py
+++ backend/wallets/views.py
@@ -1,6 +1,5 @@
 from decimal import Decimal
 
-from django.http import Http404
 from rest_framework import generics, status, views
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import get_object_or_404
